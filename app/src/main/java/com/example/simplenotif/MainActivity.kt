5a1e0f77279ebc95303df4c772fd2df9cfc889a8
Asep Sutisna <sutisnaasep323@gmail.com>
2021-09-21 20:48:19 +0700

    Finish & Comments

--- app/src/main/java/com/example/simplenotif/MainActivity.kt
+++ app/src/main/java/com/example/simplenotif/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.example.simplenotif
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private const val NOTIFICATION_ID = 1
+        private const val CHANNEL_ID = "Channel_01"
+        private const val CHANNEL_NAME = "Yummi Channel"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //aksi untuk onClick pada button
+    fun sendNotification(view: View) { // Kelebihan dari cara ini yakni kita tidak perlu menginisialisasi Button tersebut di onCreate, sehingga pemanggilan kode bisa lebih cepat.
+        val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            /*
+            Small Icon : Ikon ini yang akan muncul pada status bar (wajib ada).
+            Large Icon : Ikon ini yang akan muncul di sebelah kiri dari text notifikasi.
+            Content Intent : Intent ini sebagai action jika notifikasi ditekan.
+            Content Title : Judul dari notifikasi (wajib ada).
+            Content Text : Text yang akan muncul di bawah judul notifikasi (wajib ada).
+            Subtext : Text ini yang akan muncul di bawah content text.
+            Auto Cancel : Digunakan untuk menghapus notifikasi setelah ditekan.
+             */
+            .setSmallIcon(R.drawable.ic_notifications_white_48px)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_notifications_white_48px))
+            .setContentTitle(resources.getString(R.string.content_title))
+            .setContentText(resources.getString(R.string.content_text))
+            .setSubText(resources.getString(R.string.subtext))
+            .setAutoCancel(true)
+
+        /*
+        Untuk android Oreo ke atas perlu menambahkan notification channel agar notifikasi bisa berjalan
+        */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            /* Create or update. */
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+            channel.description = CHANNEL_NAME
+            mBuilder.setChannelId(CHANNEL_ID)
+            mNotificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = mBuilder.build()
+        /*
+        untuk mengirim notifikasi sesuai dengan id yang kita berikan. Fungsi id di sini nanti juga
+        bisa untuk membatalkan notifikasi yang sudah muncul
+         */
+        mNotificationManager.notify(NOTIFICATION_ID, notification)
     }
 }
