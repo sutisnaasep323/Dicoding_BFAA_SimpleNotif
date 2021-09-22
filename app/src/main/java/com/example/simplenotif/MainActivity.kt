0183785516af7ce40f9a3bf70c785aaa99daaa3b
Asep Sutisna <sutisnaasep323@gmail.com>
2021-09-22 20:12:48 +0700

    Notification with pending intent

--- app/src/main/java/com/example/simplenotif/MainActivity.kt
+++ app/src/main/java/com/example/simplenotif/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.simplenotif
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -25,6 +28,13 @@ class MainActivity : AppCompatActivity() {
 
     //aksi untuk onClick pada button
     fun sendNotification(view: View) { // Kelebihan dari cara ini yakni kita tidak perlu menginisialisasi Button tersebut di onCreate, sehingga pemanggilan kode bisa lebih cepat.
+
+        /*
+        Digunakan untuk memberikan action jika notifikasi disentuh.
+         */
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://dicoding.com"))
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
         val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
             /*
@@ -36,6 +46,7 @@ class MainActivity : AppCompatActivity() {
             Subtext : Text ini yang akan muncul di bawah content text.
             Auto Cancel : Digunakan untuk menghapus notifikasi setelah ditekan.
              */
+            .setContentIntent(pendingIntent) //Pending intent adalah sebuah token yang diberikan untuk aplikasi lainnya (termasuk juga aplikasi itu sendiri). Di sini kita kan menggunakan pending intent untuk mengirimkan notifikasi
             .setSmallIcon(R.drawable.ic_notifications_white_48px)
             .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_notifications_white_48px))
             .setContentTitle(resources.getString(R.string.content_title))
